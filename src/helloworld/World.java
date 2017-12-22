804b8ee937a146876ed9b43f5e9958dc76c493dc
PratikshaRane1 <ranepratiksha21@gmail.com>
2017-12-22 11:31:35 +0530

    Verify

--- src/helloworld/World.java
+++ src/helloworld/World.java
@@ -5,6 +5,8 @@ public class World {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World Alpha");
+		System.out.println("hello world Beta");
+		System.out.println("Hello world Charlie");
 	}
 
 }
